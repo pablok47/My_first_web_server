f1ec39659476471b83a7defcf628f1e7779e0a1f
pablok47 <pkaparis@gmail.com>
2015-10-12 21:08:27 -0400

    added stock picking abilty from url

--- server.rb
+++ server.rb
@@ -1,6 +1,21 @@
 require 'sinatra'
+require 'httparty'
+require 'nokogiri'
 
 get '/' do
-	File.open("./public/hello.txt").read
+  stock = params[:stock]
+  symbol = stock.downcase
+  response = HTTParty.get("http://finance.yahoo.com/q?s=#{symbol}")
+  dom = Nokogiri::HTML(response.body)
+  my_span = dom.xpath("//span[@id='yfs_l84_#{symbol}']").first.content
+  "The current stock price is $#{my_span}"
+
 end
+
+
+
+
+
+
+
 
